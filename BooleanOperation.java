bce9e93a5bb0940e7d79fb19c60b219d89cd7e2b
Pandiboina-Visali <104361104+Pandiboina-Visali@users.noreply.github.com>
2022-05-05 23:44:02 +0530

    Add files via upload

---
+++ BooleanOperation.java
@@ -0,0 +1,33 @@
+//JFM1T3_Assignment5:
+/*
+1.Write a program to initialize a boolean variable and print it's opposite value on console.
+
+  Sample Input:true
+
+  Expected Output:false
+
+*/
+import java.lang.System;
+public class BooleanOperation 
+{
+  public static void main(String args[])
+  {
+     boolean t=true; 
+     boolean f=false;
+     if(t==true)
+     {
+     System.out.println(f);
+     } 
+  } 
+}
+    
+      
+    
+    
+
+//Define main method
+
+//Declare a variable and initialize it as true or false 
+
+//Print the Result using not operator 
+
