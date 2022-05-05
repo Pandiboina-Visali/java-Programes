551dde77a317af29686d509aeaac7842c6ce28ae
Pandiboina-Visali <104361104+Pandiboina-Visali@users.noreply.github.com>
2022-05-05 23:42:42 +0530

    Add files via upload

---
+++ Varaibles.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Varaibles
+{
+  public static void main(String args[])
+  {
+    int a=10;b=20;c=30;
+    if(a==b && b==c && c==a);
+    {
+    System.out.println("all varaibles are equal");
+      }
+    else
+    {
+ System.out.println("all varaibles are not equal");
+    }
+  }
+}
+      
+    
