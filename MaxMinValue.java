c37d0d7ab44ea753e7e7c8137e3a504866cdf22c
Pandiboina-Visali <104361104+Pandiboina-Visali@users.noreply.github.com>
2022-05-06 06:48:04 +0530

    Add files via upload

---
+++ MaxMinValue.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class MaxMinValue
+  {
+    public static void main(String args[])
+    {
+     int a=10,b=5,max,min;
+     max=(a>b)?a:b;
+     min=(a<b)?a:b;
+     System.out.println("the maximum value is:"+max);
+     System.out.println("the mimimum value is:"+min);
+      }
+  }
